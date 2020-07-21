7409283c8c92a05773eea2abb024fe3e8b98185c
wradiy <wicakradityo778@gmail.com>
2020-07-21 16:04:50 +0700

    Initial Commit

---
+++ SOLID Principles/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SOLID Principles
+//
+//  Created by Crocodic on 21/07/20.
+//  Copyright © 2020 WradDev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
